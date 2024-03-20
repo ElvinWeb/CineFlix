a7db7e9d31083371973d9a8b5a96205366b139f7
Elvin <elvinsrkrov@gmail.com>
2024-03-20 16:35:21 +0400

    header section html and css design has added

---
+++ assets/JavaScript/global.js
@@ -0,0 +1,10 @@
+const addEventOnElements = function (elements, eventType, callback) {
+  for (const elem of elements) elem.addEventListener(eventType, callback);
+};
+
+const searchBox = document.querySelector("[search-box]");
+const searchTogglers = document.querySelectorAll("[search-toggler]");
+
+addEventOnElements(searchTogglers, "click", function () {
+  searchBox.classList.toggle("active");
+});
